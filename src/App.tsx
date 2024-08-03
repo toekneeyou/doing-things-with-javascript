a141a2bf32ebc4fa2ca67f741358a07f4c5ba46a
Tony <tonyyu1129@gmail.com>
2024-08-03 14:02:06 -0700

    finish Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { classnames } from "./util/classnames";
 import "./main.css";
+import Navigation from "./features/navigation/Navigation";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <div className="">App</div>;
+  return (
+    <div
+      className={classnames(
+        "bg-app-slate-blue h-screen w-full overflow-hidden text-white flex"
+      )}
+    >
+      <Navigation />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
