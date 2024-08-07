ce3fe744a9d2b7791a253efe883826054d5495d1
Tony <tonyyu1129@gmail.com>
2024-08-06 20:26:27 -0700

    use variant to style buttons in debounce and throttle view

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -39,7 +39,7 @@ export default function Button({
       className={classnames(
         "button",
         "centered w-max h-10 border-2 border-app px-4 rounded-full whitespace-nowrap text-app-dark-blue font-semibold transition-transform",
-        "disabled:bg-gray-600 disabled:pointer-events-none",
+        "disabled:pointer-events-none disabled:grayscale",
         "active:translate-y-1 active:scale-[.99]",
         {
           "bg-app-yellow": isFilled && color === yellow,
@@ -55,6 +55,14 @@ export default function Button({
           "border-app-slate-blue": isBordered && color === slateBlue,
           "border-app-dark-blue": isBordered && color === darkBlue,
           "border-app-faded-blue": isBordered && color === fadedBlue,
+          "border-transparent": !isBordered,
+        },
+        {
+          "text-app-yellow": isBordered && color === yellow,
+          "text-app-error": isBordered && color === error,
+          "text-app-slate-blue": isBordered && color === slateBlue,
+          "text-app-dark-blue": isBordered && color === darkBlue,
+          "text-app-faded-blue": isBordered && color === fadedBlue,
           "border-transparent": !isBordered,
         },
         className
