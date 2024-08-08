bf00e3346fc8d29ac239ffb018e1c427d4ebf7f5
Tony <tonyyu1129@gmail.com>
2024-08-08 14:23:38 -0700

    update styling of search result list

--- src/features/search/Search.tsx
+++ src/features/search/Search.tsx
@@ -5,6 +5,8 @@ import { ClickAwayListener } from "@mui/material";
 import { SearchResultItemProps } from "./SearchResultItem";
 import SearchResultsList from "./SearchResults";
 
+const SEARCH_RESULTS_CLASS = "search__results";
+
 interface SearchProps {
   handleChange: ChangeEventHandler<HTMLInputElement>;
   className?: string;
@@ -26,13 +28,31 @@ export default function Search({
   };
 
   useLayoutEffect(() => {
-    const observer = new ResizeObserver(() => {
+    const addBorderToResultListItems = () => {
+      const firstLis =
+        resultsRef.current?.querySelectorAll(
+          `.${SEARCH_RESULTS_CLASS} > ul > li`
+        ) ?? [];
+
+      firstLis.forEach((li, i) => {
+        if (i !== firstLis.length - 1) {
+          const liEl = li as HTMLLIElement;
+          liEl.style.borderBottom = "1px solid #475569";
+        }
+      });
+    };
+
+    const setResultsListMaxHeight = () => {
       const resultsEl = resultsRef.current!;
       const maxHeight =
         window.innerHeight - resultsEl.getBoundingClientRect().top - 32;
       resultsEl.style.maxHeight = `${maxHeight}px`;
-    });
+    };
+
+    addBorderToResultListItems();
+    const observer = new ResizeObserver(setResultsListMaxHeight);
     observer.observe(document.body);
+
     return () => {
       observer.disconnect();
     };
@@ -53,8 +73,9 @@ export default function Search({
         <div
           ref={resultsRef}
           className={classnames(
-            "search-results",
-            "absolute bg-app-slate-blue top-6 w-full pt-4 rounded-b-xl overflow-y-auto overflow-x-hidden shadow-md",
+            SEARCH_RESULTS_CLASS,
+            "absolute top-6",
+            "bg-app-dark-blue w-full pt-4 rounded-b-xl overflow-y-auto overflow-x-hidden shadow-md",
             {
               "opacity-0 pointer-events-none":
                 !showResults || queryString.length === 0,
